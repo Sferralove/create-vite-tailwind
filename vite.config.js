a4c4b7fd2ebed3258346f855532e8b81a9ba34fa
Sferralove <sferra.angelo@gmail.com>
2024-03-31 14:31:36 +0200

    Shadcn_ui configuration

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -13,6 +14,7 @@ export default defineConfig({
       '@services': '/src/services',
       '@utils': '/src/utils',
       '@views': '/src/views',
+      '@': path.resolve(__dirname, './src'),
     },
   },
 });
